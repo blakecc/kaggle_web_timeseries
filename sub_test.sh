f7c0178308d904a3c74182de2a065531273fb015
Blake Cuningham <blake.rsa@gmail.com>
2017-09-08 10:53:24 +0200

    shell script email settings

--- sub_test.sh
+++ sub_test.sh
@@ -2,8 +2,8 @@
 #PBS -l nodes=1:ppn=15:series600
 #PBS -N kag_web_sub
 
-#PBS –M blake.rsa@gmail.com
-#PBS -m aeb
+#PBS -m abe
+#PBS -M blake.rsa@gmail.com
 
 cd /home/cnnbla001/kaggle_web_timeseries
 module load software/R-3.3.2 mpi/openmpi-1.10.1
