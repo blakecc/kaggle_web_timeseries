a664230885fc4e1251c6f0ed5e365c855958da84
Blake Cuningham <blake.rsa@gmail.com>
2017-09-08 10:27:19 +0200

    shell script added for hex cluster

---
+++ sub_test.sh
@@ -0,0 +1,11 @@
+#PBS -q UCTlong
+#PBS -l nodes=1:ppn=15:series600
+#PBS -N kag_web_sub
+
+#PBS –M blake.rsa@gmail.com
+#PBS -m aeb
+
+cd /home/cnnbla001/kaggle_web_timeseries
+module load software/R-3.3.2 mpi/openmpi-1.10.1
+
+mpirun -np 15 -hostfile $PBS_NODEFILE R --slave CMD BATCH compile_submission_2_cluster.R
